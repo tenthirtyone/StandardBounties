e1ef9521c129cbe6c2722f8940ee480f05c01d60
tenthirtyone <alexsherbuck@gmail.com>
2020-11-17 15:01:43 -0500

    update solc ver

--- truffle.js
+++ truffle.js
@@ -1,17 +1,19 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const fs = require('fs')
-
+const secrets = require('./secrets');
 // First read in the secrets.json to get our mnemonic
-let secrets
+console.log(secrets)
+
 let mnemonic
-if (fs.existsSync('secrets.json')) {
-  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'))
+if (secrets) {
+  
   mnemonic = secrets.mnemonic
 } else {
   console.log('No secrets.json found. If you are trying to publish EPM ' +
               'this will fail. Otherwise, you can ignore this message!')
   mnemonic = ''
 }
+
 
 module.exports = {
   networks: {
@@ -29,6 +31,12 @@ module.exports = {
       provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
       network_id: '3',
       gas: 4600000
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + secrets.infura),
+      network_id: '4',
+      gas: 10000000,
+      gasPrice: 100000000000
     },
     testrpc: {
       network_id: 'default',
